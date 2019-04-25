6f85f52d948e32b8603056163f272cb2d54ff782
Ivan Lutsyk <ivanlutsyk87@gmail.com>
2019-04-25 18:01:16 +0300

    first commit

---
+++ Raizor/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Razor.Models;
+
+namespace Raizor.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            Product[] products =
+            {
+                new Product
+                {
+                    ProductionId = 1,
+                    Name = "Kayak",
+                    Description = "A boat for one person",
+                    Category = "Watersport",
+                    Price = 275m
+                },
+                new Product
+                {
+                    ProductionId = 2,
+                    Name = "Katamaran",
+                    Description = "A boat for many people",
+                    Category = "Watersport",
+                    Price = 500m
+                }
+            };
+
+        ViewBag.StockLevel = 2;
+            return View(products);
+        }
+    }
+}
